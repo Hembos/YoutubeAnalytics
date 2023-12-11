327755a95e2d5646bb9c61942c752eb033fdfb7b
Hembos <faizrahmanov.artur@mail.ru>
2023-12-11 23:33:05 +0300

    Ui

--- frontend_desktop/ui/download_widget.py
+++ frontend_desktop/ui/download_widget.py
@@ -2,17 +2,19 @@ from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtWidgets import QLineEdit, QWidget, QHBoxLayout, QLabel, QPushButton
 
 
-class SearchWidget(QWidget):
+class UrlWidget(QWidget):
     def __init__(self, parent: QWidget | None = None, flags: Qt.WindowFlags | Qt.WindowType = Qt.WindowFlags()) -> None:
         super().__init__(parent, flags)
-        
+
         self.__layout = QHBoxLayout()
         self.setLayout(self.__layout)
-        
+
         self.__layout.addWidget(QLabel("Введите ссылку на видео:"))
-        
-        self.__item_search_line_edit = QLineEdit()
-        self.__search_button = QPushButton("Проанализировать")
-        
-        self.__layout.addWidget(self.__item_search_line_edit)
-        self.__layout.addWidget(self.__search_button)
+
+        self.__url_line_edit = QLineEdit()
+        self.__download_button = QPushButton("Скачать")
+        self.__analyze_button = QPushButton("Проанализировать")
+
+        self.__layout.addWidget(self.__url_line_edit)
+        self.__layout.addWidget(self.__download_button)
+        self.__layout.addWidget(self.__analyze_button)
--- frontend_desktop/ui/videos_tree.py
+++ frontend_desktop/ui/videos_tree.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QTreeWidget, QTreeWidgetItem, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+
 
 class VideosTree(QWidget):
     def __init__(self):
@@ -22,3 +24,16 @@ class VideosTree(QWidget):
         self.__layout.addWidget(self.__tree)
         self.__layout.setSpacing(0)
         self.__layout.setContentsMargins(0, 0, 0, 0)
+
+    def fill(self, channels: list) -> None:
+        self.__tree.clear()
+
+        for channel in channels:
+            channelItem = QTreeWidgetItem(self.__tree)
+            channelItem.setText(0, channel["title"])
+            channelItem.setData(0, Qt.ItemDataRole.UserRole, channel["id"])
+
+            for video in channel["videos"]:
+                videoItem = QTreeWidgetItem(channelItem)
+                videoItem.setText(0, video["title"])
+                videoItem.setData(0, Qt.ItemDataRole.UserRole, video["video_id"])
