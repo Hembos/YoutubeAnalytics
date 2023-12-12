f40e39426778e10853ca3472784af21923e8ccf8
Aleksandr Shvarts <shvarcaleksandr@gmail.com>
2023-12-12 23:30:35 +0300

    Add positive and negative word maps

--- analysis/app/metrics/metric.py
+++ analysis/app/metrics/metric.py
@@ -49,7 +49,8 @@ def plot_counts_by_datetime(comments: dict, video_name: str, make_plot: bool = F
 def plot_counts_neg_and_pos(comments: dict, video_name: str, make_plot: bool = False, analyser: Analyser = None):
     if analyser is None:
         analyser = Analyser()
-    if analyser.result is None or next(iter(analyser.result.values())).get('sentiment') is None:
+    if analyser.result is None or len(analyser.result.values()) > 0\
+            and next(iter(analyser.result.values())).get('sentiment') is None:
         d_type = {'sentiment': True}
         analyser.analyse_comments(comments, d_type)
     positive_comments_count = 0
@@ -78,7 +79,8 @@ def plot_counts_neg_and_pos(comments: dict, video_name: str, make_plot: bool = F
 def plot_counts_emotion(comments: dict, video_name: str, make_plot: bool = False, analyser: Analyser = None):
     if analyser is None:
         analyser = Analyser()
-    if analyser.result is None or next(iter(analyser.result.values())).get('emotion') is None:
+    if analyser.result is None or len(analyser.result.values()) > 0 and \
+            next(iter(analyser.result.values())).get('emotion') is None:
         d_type = {'emotion': True}
         analyser.analyse_comments(comments, d_type)
     types = ['others', 'joy', 'sadness', 'anger', 'surprise', 'disgust', 'fear']
@@ -123,7 +125,8 @@ def plot_like_vs_replies_counts(comments: dict, video_name: str, make_plot: bool
 def plot_counts_langs(comments: dict, video_name: str, make_plot: bool = False, analyser: Analyser = None):
     if analyser is None:
         analyser = Analyser()
-    if analyser.result is None or next(iter(analyser.result.values())).get('lang') is None:
+    if analyser.result is None or len(analyser.result.values()) > 0 and \
+            next(iter(analyser.result.values())).get('lang') is None:
         d_type = {'lang': True}
         analyser.analyse_comments(comments, d_type)
     counts = dict()
@@ -145,25 +148,45 @@ def plot_counts_langs(comments: dict, video_name: str, make_plot: bool = False, 
 
 def plot_word_map(comments: dict, video_name: str, make_plot: bool = False, analyser: Analyser = None):
     if analyser is None:
-        analyser = Analyser()
+        analyser = Analyser()  # todo think about what if only two comments
     if analyser.result is None or \
-            (next(iter(analyser.result.values())).get('lang') is None and analyser.result.get('word_count') is None):
+            (len(analyser.result.values()) > 0 and \
+             next(iter(analyser.result.values())).get('lang') is None
+             and analyser.result.get('sentiment') is None):
         d_type = {'lang': True,
-                  'word_count': True}
+                  'sentiment': True}
         analyser.analyse_comments(comments, d_type)
-    text = analyser.result.pop('word_count')
+    # text = analyser.result.pop('word_count')
+    text = analyser.result
     counts = set()
+    pos_freq = defaultdict(int)
+    neg_freq = defaultdict(int)
+    freq = defaultdict(int)
     for entry in analyser.result.values():
+        sentance = entry['sentiment'].sentence
+        words = "".join(c for c in sentance if c.isalnum() or c.isspace())
+        words = words.lower().split()
+        for word in words:
+            freq[word] += 1
+            if entry['sentiment'].output == 'POS':
+                pos_freq[word] += 1
+            if entry['sentiment'].output == 'NEG':
+                neg_freq[word] += 1
         if entry['lang'][0]['label'] not in counts:
             counts.add(entry['lang'][0]['label'])
+
     stop_words = []
     for lang in list(counts):
         stop_words += safe_get_stop_words(lang)
     stop_words = set(stop_words)
-    freq = dict()
-    for item in text.keys():
-        if item not in stop_words:
-            freq[item] = text[item]
+
+    for item in stop_words:
+        if item in freq:
+            del freq[item]
+            if item in pos_freq.keys():
+                del pos_freq[item]
+            if item in neg_freq.keys():
+                del neg_freq[item]
     if make_plot:
         wordcloud = WordCloud(width=2000,
                               height=1500,
@@ -174,8 +197,29 @@ def plot_word_map(comments: dict, video_name: str, make_plot: bool = False, anal
                               colormap='Pastel1',
                               collocations=False,
                               stopwords=stop_words).generate_from_frequencies(freq)
-        wordcloud.to_file(f'Word_cloud_{video_name}.png')
-    return freq
+        wordcloud.to_file(f'Word_cloud_all_{video_name}.png')
+        wordcloud_pos = WordCloud(width=2000,
+                                  height=1500,
+                                  random_state=1,
+                                  background_color='black',
+                                  relative_scaling=1,
+                                  margin=20,
+                                  colormap='Pastel1',
+                                  collocations=False,
+                                  stopwords=stop_words).generate_from_frequencies(pos_freq)
+        wordcloud_pos.to_file(f'Word_cloud_pos_{video_name}.png')
+        wordcloud_neg = WordCloud(width=2000,
+                                  height=1500,
+                                  random_state=1,
+                                  background_color='black',
+                                  relative_scaling=1,
+                                  margin=20,
+                                  colormap='Pastel1',
+                                  collocations=False,
+                                  stopwords=stop_words).generate_from_frequencies(neg_freq)
+        wordcloud_neg.to_file(f'Word_cloud_neg_{video_name}.png')
+
+    return {'all_freq': freq, 'pos_freq': pos_freq, 'neg_freq': neg_freq}
 
 
 def create_all_metrics(comments: dict, video_name: str, make_plot=False):
